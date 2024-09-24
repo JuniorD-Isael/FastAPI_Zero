cce261f8261a47b2c7679ff3cc4a687c82b95c9e
JuniorD-Isael <isaeldjunior@gmail.com>
2024-09-23 21:10:45 -0300

    Criado a rota user em app.py e seu test em test_app.py

--- fast_api_zero/app.py
+++ fast_api_zero/app.py
@@ -2,9 +2,16 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
+from fast_api_zero.schemas import Message, User
+
 app = FastAPI()
 
 
-@app.get('/', status_code=HTTPStatus.OK)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, Mundo!'}
+
+
+@app.get('/user', status_code=HTTPStatus.OK, response_model=User)
+def user():
+    return {'usuario': 'fulano'}
